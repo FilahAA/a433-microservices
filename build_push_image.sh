d25daca2e7e731647028ec8ce1482a98c0458058
Filah Aditia <tech.filahaditia@gmail.com>
2022-12-12 12:10:01 +0700

    solve github login

--- build_push_image.sh
+++ build_push_image.sh
@@ -9,8 +9,8 @@ docker images
 # mengubah nama image agar sesuai dengan format Github Packages
 docker tag item-app:v1 ghcr.io/filahaa/item-app:v1
 
-# login ke Github Packages untuk keamanan saya menggunakan Token
-echo $TOKEN_GITHUB | docker login ghcr.io -u filahaa --password-stdin
+# login ke Github Packages
+echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u filahaa --password-stdin
 
 # mengunggah image ke Github Packages
 docker push ghcr.io/filahaa/item-app:v1
